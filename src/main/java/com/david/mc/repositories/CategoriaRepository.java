82a883a72e28cf61e64754dd9aad465183ded931
David Stefani Prado <daviprado1995@gmail.com>
2021-05-04 19:00:38 -0300

    Criando repository e service para a Categoria

---
+++ src/main/java/com/david/mc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.david.mc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.david.mc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+	
+
+}
