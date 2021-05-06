c9b013b0240c4f07e4a595cf8498ab79b2bcadca
David Stefani Prado <daviprado1995@gmail.com>
2021-05-06 18:52:29 -0300

    Produto e associação de muitos para muitos

---
+++ src/main/java/com/david/mc/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.david.mc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.david.mc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+	
+
+}
