88a8398b59b01ab20e582c2ab1c9353370760882
David Stefani Prado <daviprado1995@gmail.com>
2021-05-15 08:58:11 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/david/mc/CursomcApplication.java
+++ src/main/java/com/david/mc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.david.mc.domain.Cidade;
 import com.david.mc.domain.Cliente;
 import com.david.mc.domain.Endereco;
 import com.david.mc.domain.Estado;
+import com.david.mc.domain.ItemPedido;
 import com.david.mc.domain.Pagamento;
 import com.david.mc.domain.PagamentoComBoleto;
 import com.david.mc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.david.mc.repositories.CidadeRepository;
 import com.david.mc.repositories.ClienteRepository;
 import com.david.mc.repositories.EnderecoRepository;
 import com.david.mc.repositories.EstadoRepository;
+import com.david.mc.repositories.ItemPedidoRepository;
 import com.david.mc.repositories.PagamentoRepository;
 import com.david.mc.repositories.PedidoRepository;
 import com.david.mc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -111,6 +115,21 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1,2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
+		
+		
+		
 	}
-
 }
---
+++ src/main/java/com/david/mc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,13 @@
+package com.david.mc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.david.mc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+	
+	
+
+}
