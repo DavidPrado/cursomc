244fcc2d897d865419412bac22679661cdb73248
David Stefani Prado <daviprado1995@gmail.com>
2021-06-14 19:45:34 -0300

    Validação customizada de CPF e CNPJ na inserção de cliente

--- src/main/java/com/david/mc/dto/ClienteNewDTO.java
+++ src/main/java/com/david/mc/dto/ClienteNewDTO.java
@@ -2,6 +2,14 @@ package com.david.mc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.david.mc.services.validation.Cliente⁯Insert;
+
+@Cliente⁯Insert
 public class ClienteNewDTO implements Serializable {
 	
 	/**
@@ -9,17 +17,29 @@ public class ClienteNewDTO implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Length(min=5,max=120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Email(message = "E-mail inválido")
 	private String email;
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String logradouro;
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String numero;
 	private String complemento;
 	private String bairro;
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String cep;
 	
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
