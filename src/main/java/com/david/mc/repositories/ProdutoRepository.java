f6e1bee682937028fe088ac0c3e5019c8f177ff8
David Stefani Prado <daviprado1995@gmail.com>
2021-06-28 19:21:01 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/david/mc/repositories/ProdutoRepository.java
+++ src/main/java/com/david/mc/repositories/ProdutoRepository.java
@@ -1,13 +1,22 @@
 package com.david.mc.repositories;
 
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.david.mc.domain.Categoria;
 import com.david.mc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 	
 	
-
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome,List<Categoria> categorias, Pageable pageRequest);
+	
 }
